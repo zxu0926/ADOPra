84ec4ce6fe220601cfc2c37102c2443e52915bee
zhangxu <zhangx0926@gmail.com>
2024-10-20 15:21:33 +0800

    add function

--- utils/string_util.cpp
+++ utils/string_util.cpp
@@ -1,7 +1,18 @@
+#include "string_util.h"
+#include <windows.h>
+
 string_util::string_util()
 {
 }
 
 string_util::~string_util()
 {
+}
+
+std::string& string_util::trim_l(std::string& str)
+{
+	if (str.empty()) return str;
+
+	str.erase(0, str.find_first_not_of(" \n\r\t"));
+	return str;
 }
--- utils/string_util.h
+++ utils/string_util.h
@@ -1,10 +1,14 @@
 
 #pragma once
+#include <string>
 
 class string_util
 {
 public:
 	string_util();
 	~string_util();
+
+public:
+	static std::string& trim_l(std::string& str);
 };
 

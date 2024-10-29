c83e7080c140f9ac004105317ccf686cd9c5642e
zhangxu <41820158+zxu0926@users.noreply.github.com>
2024-10-29 14:09:21 +0800

    Update string_util.h
    add to_upper function

--- utils/string_util.h
+++ utils/string_util.h
@@ -29,5 +29,7 @@ public:
 	static std::string& trim_r(std::string &str);
 	static std::string& trim(std::string &str);
         static std::string& reverse(std::string &str);
+
+	static std::string& to_upper(std::string &str);
 };
 

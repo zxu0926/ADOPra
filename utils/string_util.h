23ade3bb7642044aab275b38f4b5664638b5457e
zhangxu <41820158+zxu0926@users.noreply.github.com>
2024-11-20 13:56:45 +0800

    Update string_util.h

--- utils/string_util.h
+++ utils/string_util.h
@@ -48,5 +48,6 @@ public:
 	static bool is_empty(std::string &str);
 
 	static std::string format(const std::string fmt, ...);
+	static std::string& append_format(std::string& str, const std::string fmt, ...);
 };
 

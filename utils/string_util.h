831b4933ae53b6f86ed0e73ee520ce0ecbc51031
zhangxu <41820158+zxu0926@users.noreply.github.com>
2024-11-27 14:16:49 +0800

    Update string_util.h

--- utils/string_util.h
+++ utils/string_util.h
@@ -51,5 +51,6 @@ public:
 	static std::string& append_format(std::string& str, const std::string fmt, ...);
 	static std::string& erase(std::string& str, char ch);
 	static bool replace(std::string& str, const std::string& from, const std::string& to);
+	static void replace_all(std::string& str, const std::string& from, const std::string& to);
 };
 
